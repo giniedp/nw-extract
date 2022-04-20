77da753bc0eac2e291ca5ae5eb17ee60553e1a38
Ginie <github@ginie.eu>
2022-04-20 09:13:13 +0200

    added xml extract option

--- src/lib/extract.ts
+++ src/lib/extract.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs'
 import * as path from 'path'
-import { isDatasheet, isIcon, isImage, isImageFile, isLocale, isScript, isTexture, listFiles } from './listFiles'
+import { isDatasheet, isIcon, isImage, isImageFile, isLocale, isScript, isTexture, isXML, listFiles } from './listFiles'
 import { decompress, DecompressEntry } from './decompress'
 import { listPakFiles } from './globFiles'
 import { Entry } from 'yauzl'
@@ -10,7 +10,7 @@ import { convertLocale } from './convertLocale'
 import { convertImage } from './convertImages'
 import { pathIsFile } from './copy'
 
-export type AssetType = 'datasheet' | 'locale' | 'texture' | 'icon' | 'image' | 'script'
+export type AssetType = 'datasheet' | 'locale' | 'texture' | 'icon' | 'image' | 'script' | 'xml'
 export type AssetFormat = 'csv' | 'json' | 'xml' | 'png' | 'jpg'
 export type AssetFilter = `${AssetType}:${AssetFormat}`
 
@@ -37,10 +37,7 @@ interface AssetConversion {
 }
 
 export async function extract({ inputDir, outputDir, filter, onProgress, update }: ExtractOptions) {
-  const stat = await fs.stat(inputDir)
   const conversion = parseFilter(filter)
-  const glob = path.join(inputDir, '**', '*.pak')
-  // const pakFiles = await globFiles(glob)
   const pakFiles = await listPakFiles(inputDir)
   const entryFilter = createFilter(conversion)
   const entryConverter = createConverter(conversion)
@@ -129,6 +126,9 @@ function createFilter(conversion: AssetConversion[]) {
       return true
     }
     if (filter.includes('script') && isScript(entry.fileName)) {
+      return true
+    }
+    if (filter.includes('xml') && isXML(entry.fileName)) {
       return true
     }
     return false
